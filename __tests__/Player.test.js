505c2f0cb0babef90ed6e2b0a3e1884b122a6729
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-11 11:58:08 -0700

    added methods and using prototype instead of this

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,6 +3,7 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 console.log(new Potion());
 
+// test for the Player object
 test ('creates a player object', () => {
   const player = new Player('Dave');
 
@@ -12,3 +13,27 @@ test ('creates a player object', () => {
   expect(player.agility).toEqual(expect.any(Number));
   expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
+
+
+// test for the getStats() method
+test ("get player's stats as an object", () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility'); 
+});
+
+
+// test for getInventory() method
+test ('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
+
